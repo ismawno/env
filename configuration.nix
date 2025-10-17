100259da1e154a73fb00bde2c9da9df033e7e4ce
Ismael Bueno <ismaelwno@gmail.com>
2025-10-17 23:10:35 +0200

    fix: Remove redundant import

--- configuration.nix
+++ configuration.nix
@@ -5,10 +5,6 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
 
 nix.settings.experimental-features = [ "nix-command" "flakes"];
 

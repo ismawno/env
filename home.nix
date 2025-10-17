a4a66b8f3c344231ee40533533137ed0823e2ebf
Ismael Bueno <ismaelwno@gmail.com>
2025-10-17 23:20:22 +0200

    fix: Malformed home

--- home.nix
+++ home.nix
@@ -6,10 +6,10 @@
 
   programs.git.enable = true;
 
-  home.file.".zshrc".source = ./zsh/.zshrc
-  home.file.".p10k.zsh".source = ./zsh/.p10k.zsh
+  home.file.".zshrc".source = ./zsh/.zshrc;
+  home.file.".p10k.zsh".source = ./zsh/.p10k.zsh;
 
-  home.file.".tmux.conf".source = ./tmux/.tmux.conf
+  home.file.".tmux.conf".source = ./tmux/.tmux.conf;
 
   home.file.".config/hypr".source = ./hyprland/.config/hypr;
   home.file.".config/ghostty".source = ./ghostty/.config/ghostty;

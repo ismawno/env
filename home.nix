91d6608a9ee9cf67c843cf882f4251b194bad28f
Ismael Bueno <ismaelwno@gmail.com>
2025-10-18 22:11:55 +0200

    rebuild: NixOS configuration generation: 9

--- home.nix
+++ home.nix
@@ -16,6 +16,12 @@ in {
     ".zshrc".source = ./zsh/.zshrc;
     ".p10k.zsh".source = ./zsh/.p10k.zsh;
     ".tmux.conf".source = ./tmux/.tmux.conf;
+    ".tmux/plugins/tpm".source = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tpm";
+      rev = "v3.1.0"; # check the latest release
+      sha256 = "18i499hhxly1r2bnqp9wssh0p1v391cxf10aydxaa7mdmrd3vqh9";
+    };
 
     ".config/nvim".source = inputs.nvim;
     ".config/hypr/hyprland.conf".source = ./hyprland/.config/hypr/hyprland.conf;

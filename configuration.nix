1e59ba4a4cfda7ca6736bd4164083f8ee8d77c5c
Ismael Bueno <ismaelwno@gmail.com>
2025-10-18 17:03:54 +0200

    rebuild: NixOS configuration generation: 25

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,7 @@
 
   services.xserver.enable = false;
   services.displayManager.ly.enable = true;
+  services.openssh.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.

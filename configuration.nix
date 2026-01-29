459898fee2220b46a4e394fea1f8b73e20335ee5
Mars-Wave <tomas@protonmail.com>
2026-01-29 18:44:30 +0100

    rebuild: NixOS hostname generation for host 'smalltop' - 15

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,12 @@
     wireplumber.enable = true;
   };
 
+  services.samba = {
+    enable = true;
+    winbindd.enable = true;
+  };
+
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

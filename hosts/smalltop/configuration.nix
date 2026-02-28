124a123c206c06a2743e2afb1630ce996450b7a6
Mars-Wave <tomas@protonmail.com>
2026-02-28 19:52:02 +0100

    rebuild: NixOS hostname generation for host 'smalltop' - 17

--- hosts/smalltop/configuration.nix
+++ hosts/smalltop/configuration.nix
@@ -45,6 +45,15 @@
 
   swapDevices = [ ];
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  };
+
   networking.hostName = "smalltop";
 
   users.users.maddev = {
